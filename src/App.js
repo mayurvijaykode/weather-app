9f93fa81b9891829d7314b8d180bd90f31c1173e
mayurvijaykode <127609492+mayurvijaykode@users.noreply.github.com>
2023-03-14 10:13:37 +0530

    layout - form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Container>
+      <h1>Weather App</h1>
+      <p>Get weather infomration of your desired location</p>
+
+      <Row>
+        <Col>
+          <Form.Control type="text" placeholder="Enter Location" />
+        </Col>
+        <Col>
+          <Button variant="primary">Search</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
