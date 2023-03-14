0cc64dffc5134286e9dc5c190a01ac272af8021e
mayurvijaykode <127609492+mayurvijaykode@users.noreply.github.com>
2023-03-14 10:29:04 +0530

    configures API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
@@ -6,7 +6,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 
+// import axios from 'axios';
+
+const APIKey = 'bde9d434aae08ecaf12cf048a0847bab';
+
 export default function App() {
+  const [location, setLocation] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const getWeatherDetails = async () => {
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`
+    );
+    let json = await response.json();
+    setWeather(json);
+    console.log(weather);
+  };
+
+  const searchHandler = async () => {
+    await getWeatherDetails();
+  };
+
   return (
     <Container>
       <h1>Weather App</h1>
@@ -14,12 +34,19 @@ export default function App() {
 
       <Row>
         <Col>
-          <Form.Control type="text" placeholder="Enter Location" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </Col>
         <Col>
-          <Button variant="primary">Search</Button>
+          <Button variant="primary" onClick={searchHandler}>
+            Search
+          </Button>
         </Col>
       </Row>
+      {/* {location} */}
     </Container>
   );
 }
